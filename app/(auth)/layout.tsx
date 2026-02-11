6f318088324296db1ed020669481eaf9536238d1
purusottam32 <kingspuru@gmail.com>
2026-02-12 00:17:48 +0530

    feat: create auth layout component to center children

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const layout = ({children}: {children: React.ReactNode}) => {
+const layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='flex justify-center pt-20 pb-5'>{children}</div>
+    <div className='flex items-center justify-center min-h-screen'>{children}</div>
   )
 }
 
